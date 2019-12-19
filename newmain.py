f9a5cf7ed9d80eee381134edbc38efbba0a6eff5
zachzoref <zachzoref@gmail.com>
2019-12-19 17:50:42 +0200

    fix

--- newmain.py
+++ newmain.py
@@ -7,25 +7,27 @@ t2_json = functions.t_read("tree2.txt")
 print(json.dumps(t1_json, indent=4, sort_keys=True))
 print(json.dumps(t2_json, indent=4, sort_keys=True))
 
+
 def get_keys(arr):
-    newarr = []
+    new_arr = []
     for i in arr:
         if type(i) != dict:
-            newarr.append(i)
+            new_arr.append(i)
         else:
-            newarr.append(next(iter(i)))
-    return newarr
+            new_arr.append(next(iter(i)))
+    return new_arr
+
 
 def new_func(t1, el):
-    print("t1:",t1,"el:",el)
-    foundbasic=False
-    foundMiddle=False
-    final=""
-    missing=[]
+    # print("t1:", t1, "el:", el)
+    found_basic = False
+    found_middle = False
+    # final = ""
+    missing_dict = {}
     for i in t1:
         if type(i) == type(el):
             if i == el:
-                print("found one, returning")
+                # print("found one, returning")
                 return True
             elif type(i) == dict:
                 el_key = next(iter(el))
@@ -33,36 +35,33 @@ def new_func(t1, el):
                 if i_key == el_key:
                     if type(i[i_key]) != list:
                         i[i_key] = [i[i_key]]
-                        print(i[i_key],type(i[i_key]))
+                        # print(i[i_key], type(i[i_key]))
+                    list_of_missing = []
                     for j in el[el_key]:
-                        value = new_func(i[i_key],j)
-                        print("the value is:",value)
+                        value = new_func(i[i_key], j)
+                        # print("the value is:", value)
                         if value != True:
-                            if type(value) == list:
-                                for k in value:
-                                    mystr = el_key + " => " + k
-                                    missing.append(mystr)
-                            elif type(value) == dict:
-                                missing.append(value)
-                            else:
-                                    mystr = el_key + " => " + value
-                                    missing.append(mystr)
-                            foundbasic = True
-                            foundMiddle = True
+                            list_of_missing.append(value)
+                            found_basic = True
+                            found_middle = True
                         else:
-                            foundbasic = True
-    if foundbasic != True:
-        print("returning el:",el)
+                            found_basic = True
+                    missing_dict[el_key] = list_of_missing
+    if not found_basic:
+        # print("returning el:", el)
         return el
-    elif foundMiddle:
-        return missing
+    elif found_middle:
+        # print("returning missing noderrrrr:", missing_dict)
+        return missing_dict
     else:
         return True
+
 
 def compare_arr(t2, t1):
     for i in t1:
         value = new_func(t2, i)
         if value != True:
             print("yay", value)
+
 
 compare_arr(t1_json, t2_json)
