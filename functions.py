39f44c9ecad8a683dc2a6cf032b69c8e7bb4b633
Zach Zoref <zachzoref@gmail.com>
2019-12-20 01:40:12 +0200

    error handling in the json.load function

--- functions.py
+++ functions.py
@@ -3,7 +3,12 @@ import json
 
 def t_read(file_name):
     with open(file_name, "r") as pointer:
-        return json.load(pointer)
+        try:
+            return json.load(pointer)
+        except:
+            print("There was an error converting the content of {} ".format(file_name) +
+                  "into a Python json object, please check the content and try again.")
+            exit(1)
 
 def t_write(content, file_name):
     with open(file_name, "w+") as pointer:
